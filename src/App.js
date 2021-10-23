da5fb2c01c1856112cc5ea87d1ee78a1723a5338
Tusharshrivastav <ts3657@gmail.com>
2021-10-23 20:04:07 +0530

    finsh

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+import { Signup } from './components/Signup';
+
+function App() {
+  return (
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-md-5">
+          <Signup />
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
